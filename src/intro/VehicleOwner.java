928d6703b69089151980658f7c86bfb882842cad
Daniil <dailji@ttu.ee>
2023-03-13 23:40:10 +0200

    try

--- src/intro/VehicleOwner.java
+++ src/intro/VehicleOwner.java
@@ -1,48 +1,118 @@
 package intro;
 
-import oo.hide.Point;
+import java.net.*;
+import java.util.Scanner;
 
-public class Program {
 
+import java.io.*;
+import java.math.*;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class VehicleOwner {
+    private int vehicles;
+    public VehicleOwner(int vehicles){
+        this.vehicles = vehicles;
+    }
+
+    public int getVehicles() {
+        return vehicles;
+    }
     public static void main(String[] args) {
+        List<VehicleOwner> owners = new ArrayList<>(Arrays.asList(
+                new VehicleOwner(2),
+                new VehicleOwner(7),
+                new VehicleOwner(12),
+                new VehicleOwner(24),
+                new VehicleOwner(37),
+                new VehicleOwner(40)
+        ));
 
-        Double u = Double.parseDouble("24.5");
+        HashSet<Number> hs = new HashSet<Integer>();
+        HashSet<? super ClassCastException> set = new HashSet<Exception>();
+        List<String> list = new Vector<String>();
+        List<Object> values = new HashSet<Object>();
+        List<Object> objects = new ArrayList<? extends Object>();
+        Map<String, ? extends Number> hm = new HashMap<String, Integer>();
+
+        System.out.println(VehicleOwner.sortOwnersByNumberOfOwnedVehicles(owners).stream().map(each -> each.vehicles).toList());
     }
 
-    public static String asString(int num) {
-        String str = "";
-        while (num > 0){
-            String number = Integer.toString(num % 2);
-            str = number + str;
-           num = num / 2;
-        }
-        return str;
+    public static List<VehicleOwner> sortOwnersByNumberOfOwnedVehicles (List<VehicleOwner> vehicleOwners){
+        return vehicleOwners.stream().sorted(Collections.reverseOrder(Comparator.comparing(each -> each.vehicles))).toList();
     }
-
-    public static int asDecimal(String str) {
-        int answer = 0;
-        for (int i = str.length();i > 0; i--) {
-            if (str.charAt(i - 1) == '1') {
-                int c = str.length() - i;
-                answer += pow(2, c);
-            }
-        }
-        return answer;
-    }
-
-    private static int pow(int arg, int power) {
-        // Java has Math.pow() but this time write your own impleme ntation.
-        if (power == 0){
-            return 1;
-        }
-        int answ = 1;
-        for (int i = 1; i < power + 1; i++) {
-            answ = answ * arg;
-        }
-        return answ;
-    }
-
 }
+
+
+
+
+
+//
+//import fp.Map;
+//import oo.hide.Point;
+//
+//import java.time.LocalDate;
+//import java.time.format.DateTimeFormatter;
+//import java.util.function.Function;
+
+
+//public class Program {
+//
+//    public static void main(String[] args) {
+//        LocalDate d = LocalDate.parse("2022-04-30", DateTimeFormatter.ISO_LOCAL_DATE);
+//
+//        Function<Integer, String> a;
+//
+//        a = (b) -> "Hello";
+//
+//
+//        System.out.println(5/4);
+//    }
+//
+//    public static String asString(int num) {
+//        String str = "";
+//        while (num > 0){
+//            String number = Integer.toString(num % 2);
+//            str = number + str;
+//           num = num / 2;
+//        }
+//        return str;
+//    }
+//
+//    public static int asDecimal(String str) {
+//        int answer = 0;
+//        for (int i = str.length();i > 0; i--) {
+//            if (str.charAt(i - 1) == '1') {
+//                int c = str.length() - i;
+//                answer += pow(2, c);
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    private static int pow(int arg, int power) {
+//        // Java has Math.pow() but this time write your own impleme ntation.
+//        if (power == 0){
+//            return 1;
+//        }
+//        int answ = 1;
+//        for (int i = 1; i < power + 1; i++) {
+//            answ = answ * arg;
+//        }
+//        return answ;
+//    }
+//
+//}
 //package oo.hide;
 //
 //
@@ -184,7 +254,6 @@ public class Program {
 //        return newOne;
 //    }
 //}
-
 
 
 //package oo.hide;
